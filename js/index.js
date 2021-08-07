db4e3dffc6f725280015d5592d09b946dc253876
jlstevens42 <79382878+jlstevens42@users.noreply.github.com>
2021-08-07 14:17:05 -0600

    HW19 Updates
    Interaction in Skills/Tools Section

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log("Your index.js file is loaded correctly!");
+
+$("#skillsdrop").click(function() {
+    $("#skillslist").slideToggle("slow")
+});
+$("#toolsdrop").click(function() {
+    $("#toolslist").slideToggle("slow")
+});
